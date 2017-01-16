82b790f56a5c59da95b4bbf4d9178a9ad494e540
pranavgupta1234 <pranavgupta4321@gmail.com>
2017-01-16 15:51:45 +0530

    combine Latest with list of observables added

--- app/src/main/java/pranav/apps/amazing/rxoperators/Combining/CombineLatest.java
+++ app/src/main/java/pranav/apps/amazing/rxoperators/Combining/CombineLatest.java
@@ -8,25 +8,47 @@ import java.util.List;
 import pranav.apps.amazing.rxoperators.BaseActivity;
 import rx.Observable;
 import rx.Subscriber;
+import rx.Subscription;
 import rx.functions.Action1;
+import rx.functions.Func2;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by Pranav Gupta on 1/16/2017.
  */
 
 public class CombineLatest extends BaseActivity{
+    private Subscription _subscription;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mLButton.setText("CombineLsit");
-        mLButton.setOnClickListener(view -> combineListObservables().subscribe(new Action1<Integer>() {
+        mLButton.setText("CombineList");
+        mLButton.setOnClickListener(view -> {
+            mResultView.setText("");
+            if(_subscription!=null){
+                _subscription.unsubscribe();
+            }
+            _subscription = combineListObservables().subscribe(new Action1<Integer>() {
             @Override
             public void call(Integer integer) {
                 log("CombineList :" +integer);
             }
-        }));
-        
+        });});
+        mRButton.setText("CombineLatest");
+        mLButton.setOnClickListener(view -> {
+            mResultView.setText("");
+            if(_subscription!=null){
+                _subscription.unsubscribe();
+            }
+            _subscription = combineLatest().subscribe(new Action1<Integer>() {
+            @Override
+            public void call(Integer integer) {
+                log("Combine Latest: "+integer);
+            }
+        });});
     }
+
     private Observable<Integer> createObserver(int index){
         return Observable.create(new Observable.OnSubscribe<Integer>() {
             @Override
@@ -40,17 +62,33 @@ public class CombineLatest extends BaseActivity{
                     }
                 }
             }
+        }).subscribeOn(Schedulers.newThread());
+    }
+    /** Combines two source Observables by emitting an item that aggregates the latest values of each of the source Observables
+     *  each time an item is received from either of the source Observables, where this aggregation is defined by a specified function.
+     * called aggregate function
+     * */
+    private Observable<Integer> combineLatest(){
+        return Observable.combineLatest(createObserver(1), createObserver(2), (integer, integer2) -> {
+            log("left:" + integer + " right:" + integer2);
+            return integer + integer2;
         });
     }
+
     List<Observable<Integer>> list = new ArrayList<>();
     /** Combines a list of source Observables by emitting an item that aggregates the latest values of each of the
      * source Observables each time an item is received
      *  from any of the source Observables, where this aggregation is defined by a specified function called aggregate function
      * */
     private Observable<Integer> combineListObservables() {
+
+        /** Creates a list of integer observables
+         * */
          for(int i=1;i<5;i++){
              list.add(createObserver(i));
          }
+        /** Combining all observables in list and here before combining into single observable it gets and sums all the items in
+         * the list and in this it adds to 10 i.e temp =10*/
          return Observable.combineLatest(list,args ->{
              int temp = 0;
              for (Object i : args) {
@@ -60,5 +98,13 @@ public class CombineLatest extends BaseActivity{
              return temp;
          });
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(!_subscription.isUnsubscribed()) {
+            _subscription.unsubscribe();
+        }
     }
 }
