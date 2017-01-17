6c1fc017baaa4161f20ca500a8eceb8c93804c60
pranavgupta1234 <pranavgupta4321@gmail.com>
2017-01-17 12:54:20 +0530

    throttle with timeout with doc added

--- app/src/main/java/pranav/apps/amazing/rxoperators/Filtering/DebounceActivity.java
+++ app/src/main/java/pranav/apps/amazing/rxoperators/Filtering/DebounceActivity.java
@@ -19,7 +19,9 @@ public class DebounceActivity extends BaseActivity {
         mRButton.setText("debounce");
         mRButton.setOnClickListener(e -> debounceObserver().subscribe(i -> log("debounce:" + i)));
     }
-
+    /**debounce operator emits an item from an observable only is a particular timespan has passed with emitting another item, so
+     * if an item in continuosly emitted
+     * */
     private Observable<Integer> debounceObserver() {
         return Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9).debounce(integer -> {
             log(integer);
@@ -41,7 +43,16 @@ public class DebounceActivity extends BaseActivity {
                 .observeOn(AndroidSchedulers.mainThread());
 
     }
-
+    /** please check marble diagram for better understanding throttleWithTimeout(x)
+     * In this case 0 emitted wait 300, 1 emitted wait 100 , 2 emitted wait 100 , 3 emitted wait 300 , 4 emitted wait 100 , 5 emitted
+     * wait 100, 6 emitted wait 200 ..... i.e after each n%3 == 0 we have to wait 300ms otherwise 100sec , also out throttle window
+     * is of 200ms.
+     * Let us start at t=0;
+     * Now as soon as 0 emitted window of 200ms is considered and no other item is emitted during this duration so 0 is logged
+     * Now at t = 300ms 1 is passes and in timer window of 200ms extending from 300ms to 500ms we have 2 emitted at 400ms so by
+     * rule of throttle timeout an other item is emitted in the specified time window so it will not log this and same happens with 2
+     * because in its time window 3 is emitted but 3 gets logged because in its time window nothing is emitted
+     * */
     private Observable<Integer> createObserver() {
         return Observable.create(new Observable.OnSubscribe<Integer>() {
             @Override
